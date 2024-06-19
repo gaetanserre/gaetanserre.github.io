95b91bffb0656d93fff931bf9cfd634fbd9ffd3d
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2024-06-19 17:21:47 +0200

    Webring

--- links.js
+++ links.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
            && (obj.hostname != location.hostname)
            && !obj.href.match(/^javascript\:/)
            && !obj.href.match(/^$/)
-           && !obj.classList.contains("ring")
+           && !obj.classList.contains("ring-link")
   };
   
   $('a:external').attr('target', '_blank');
