16028c84e998c81b4b8c82e75bb2bd54d0a1875c
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2025-11-05 02:06:52 +0100

    video compression

--- assets/static/projects/GOB/render_anim.sh
+++ assets/static/projects/GOB/render_anim.sh
@@ -2,4 +2,4 @@ set -e -x
 
 manim -r "500,500" GOB_manim.py --disable_caching
 
-ffmpeg -i media/videos/GOB_manim/500p60/GOB.mp4 -c:v libvpx-vp9 -crf 30 -b:v 0 -b:a 128k -c:a libopus ./GOB.webm
+ffmpeg -i media/videos/GOB_manim/500p60/GOB.mp4 -c:v libvpx-vp9 -crf 50 -b:v 0 -b:a 128k -c:a libopus ./GOB.webm
